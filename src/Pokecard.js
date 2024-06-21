52eb6663c943c3a4f36b0b9321a6270639c58764
Prasad Medipudi <medipudi.prasad@gmail.com>
2024-06-21 14:38:15 +0530

    Random Pokemon Loading

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -15,11 +15,22 @@ const Styles = styled.div`
   }
 `;
 
-const POKE_API =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+// const POKE_API =
+//   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+let ThreeDigit = (Number) => (Number <= 999 ? `00${Number}`.slice(-3) : Number);
+
+// The slice() method returns selected elements in an array, as a new array.
+//.slice(start,end)
+//.slice(1, 3) -  gives items between 1-3 means not 1 it gives 2 & 3 items of an array
+//slice(-3, -1) -  gives items between 3-4 means 3 & 4 it gives
+// The slice() method selects from a given start, up to a (not inclusive) given end.
+// The slice() method does not change the original array.
 
 function Pokecard(props) {
-  let imgsrc = `${POKE_API}${props.id}.png`;
+  let imgsrc = `${POKE_API}${ThreeDigit(props.id)}.png`;
   return (
     <Styles>
       <div className="Pokecard">
